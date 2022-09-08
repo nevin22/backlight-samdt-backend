7296f073144934b1fb4cba9e20e324a9be928bea
nev_DEV <gabrielnevin@gmail.com>
2022-09-08 17:00:23 +0800

    asd

--- routes/detections.js
+++ routes/detections.js
@@ -2,23 +2,32 @@ var express = require("express");
 var router = express.Router();
 const db = require("../databricks");
 
-// router.get("/test", async(req, res) => {
-//     let data = await db.execute_query(`
-//         select * from bronze.vianalite_snapper_results_v2_0
-//         limit 10
-//     `);
-
-//     // console.table(data)
-
-//     res.status(200).json({
-//         data,
-//         message: "Success",
-//     });
-// })
-
 router.get("/:page", async (req, res) => {
-  const offset = (req.params.page - 1) * 100;
-  let has_recent = false;
+  const filters = {
+    serial_id: req.query.serial_id || undefined,
+    oid: req.query.oid || undefined,
+    EDTdate: req.query.EDTdate || undefined,
+    playback: req.query.playback || undefined
+  };
+  let debug_mode = req.query.serial_id === 'debug_mode';
+
+  let filters_query = [];
+  Object.keys(filters).forEach(d => {
+    if (filters[d] && filters[d] !== undefined) {
+      if (d === 'serial_id') {
+        if (debug_mode) {
+          filters_query = [`and (track_object ->> 'is_test') = 'true'`]
+        } else {
+          filters_query.push(`and ((track_object ->> 'is_test') = 'false' OR (track_object ->> 'is_test') is null)`);
+          filters_query.push(`and sq.serial_id = '${filters[d]}'`)
+        }  
+      } else if (d === 'EDTdate') {
+        filters_query.push(`and sq.timestamp_date < '${filters[d]}'`)
+      } else if (d === 'oid') {
+        filters_query.push(`and sq.oid = '${filters[d]}'`)
+      }
+    }
+  })
 
   if (req.params.page > 1) {
     has_recent = true
